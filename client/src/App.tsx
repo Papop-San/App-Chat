1642322bebfbb21e006ed41f32cb1409b6074f64
Papop-San <008khun159@gmail.com>
2025-05-19 15:54:15 +0700

    update: socket emit

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,24 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Routes, Route, Link } from "react-router-dom";
+import Chatroom from './chat/Chatroom';
 
-export default class App extends Component {
+class App extends Component {
+  state = { 
+    name: "John Doe"
+  };
+
   render() {
     return (
-      <div>App</div>
-    )
+      <div>
+  
+        <Link to="/chat"><button>Show Chat</button></Link>
+
+        <Routes>
+        <Route path="/chat" element={<Chatroom />} />         
+        </Routes>
+      </div>
+    );
   }
 }
+
+export default App;
--- client/src/chat/Chatroom.tsx
+++ client/src/chat/Chatroom.tsx
@@ -3,25 +3,42 @@ import { io, Socket } from "socket.io-client";
 
 interface ChatroomState {
   socket: Socket | null;
+  count: number;
 }
 
 export default class Chatroom extends Component<{}, ChatroomState> {
 
   state: ChatroomState = {
     socket: null,
+    count: 0
   };
 
   componentDidMount() {
     const socket: Socket = io("http://localhost:8080");
-    this.setState({socket:socket});  
+    this.setState({ socket });
+    // Listen for the "count" event from the server
+    socket.on("count", (data) => {
+      this.setState({ count: data.count });
+    });
+  }
 
-    socket.emit("emit", {message: "test"})
- 
+  onClick = () => {
+    // Emit an event to the server when the button is clicked
+    const { socket } = this.state;
+    if (socket) {
+      socket.emit("emit", {});  
+    } else {
+      console.error("Socket not connected yet.");
+    }
   }
 
   render() {
     return (
-      <div>Chatroom</div>
+      <div>
+        <h1>Chat Room</h1>
+        <button onClick={this.onClick}>Count</button>
+        <p>Count: {this.state.count}</p>
+      </div>
     );
   }
 }
